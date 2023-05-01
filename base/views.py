74b3bc6bf46598552ef2f4347bc625ee6a1a419a
dipteshh <dmohanta_be20@thapar.edu>
2023-05-01 23:21:35 +0530

    database & admin panel

--- base/views.py
+++ base/views.py
@@ -1,25 +1,17 @@
 from django.shortcuts import render
+from .models import Room
 
-
-rooms = [
-    {'id': 1, 'name': 'Learn Python'},
-    {'id': 2, 'name': 'Design'},
-    {'id': 3, 'name': 'Web development'},
-]
 
 # Create your views here.
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, "base/home.html", context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-            break
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, "base/room.html", context)
